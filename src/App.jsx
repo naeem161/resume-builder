084e0873da12e98e0c914de401ed2038579b0f2a
naeem161 <naeem.tahir2810@gmail.com>
2023-06-12 22:04:40 +0500

    showing forms according to currentActiveStep of stepper

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import MultiStepper from "./Components/MultiStepper";
 import Profile from "./Components/Profile";
+import Work from "./Components/Work";
+import Education from "./Components/Education";
+import Skills from "./Components/Skills";
+import Interests from "./Components/Interests";
+import Preview from "./Components/Preview";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { activeStep } = useSelector((store) => store.stepper);
+
+  // function for render all the froms according to the 'Active Step'
+  const renderForm = (currentActiveStep) => {
+    switch (currentActiveStep) {
+      case 0:
+        return <Profile />;
+      case 1:
+        return <Work />;
+      case 2:
+        return <Education />;
+      case 3:
+        return <Skills />;
+      case 4:
+        return <Interests />;
+      case 5:
+        return <Preview />;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <>
-      <h1>App Component</h1>
       <MultiStepper />
-      <Profile />
+      {renderForm(activeStep)}
     </>
   );
 }
