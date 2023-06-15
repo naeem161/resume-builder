3ed0e1b61e8eb5b07e2914a4c54ef217f39f2fe0
naeem161 <naeem.tahir2810@gmail.com>
2023-06-15 15:01:50 +0500

    make the skills form

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,5 +1,57 @@
+import { TagsInput } from "react-tag-input-component";
+import { InputLabel, Button } from "@mui/material";
+import { useFormik } from "formik";
+
 const Skills = () => {
-  return <div>Skills</div>;
+  const formik = useFormik({
+    initialValues: {
+      skills: [],
+    },
+
+    onSubmit: (values) => {
+      // saving skills in slice
+      if (formik.isValid) {
+        console.log(values);
+      }
+    },
+  });
+
+  return (
+    <>
+      <div style={{ margin: "40px 20px 0px 20px" }}>
+        <InputLabel htmlFor="skills" className="text-input">
+          Skills
+        </InputLabel>
+        <TagsInput
+          name="skills"
+          id="skills"
+          placeHolder="enter skills"
+          onChange={(value) => {
+            formik.setFieldValue("skills", value);
+          }}
+          formik={formik}
+          onBlur={formik.handleBlur}
+          value={formik.values.skills}
+        />
+      </div>
+
+      {/* Next & previous button  */}
+      <div className="btn-container">
+        <div>
+          <Button color="inherit">Back</Button>
+        </div>
+        <div>
+          <Button
+            onClick={() => formik.handleSubmit()}
+            type="submit"
+            variant="contained"
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Skills;
