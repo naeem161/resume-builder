bd5905cb5e1f56754b6fb42644d1d06b5832190e
naeem161 <naeem.tahir2810@gmail.com>
2023-06-15 23:37:37 +0500

    making Preview UI (wip)

--- src/Components/Preview.jsx
+++ src/Components/Preview.jsx
@@ -1,5 +1,30 @@
+// import { Container, Box } from "@mui/material";
+import styles from "./Preview.module.css";
+
 const Preview = () => {
-  return <div>Preview</div>;
+  return (
+    <div
+      className={styles.container}
+      style={{ margin: "40px auto", border: "2px solid red" }}
+    >
+      <div className={styles.resumeRow}>
+        {/* ---------------------------sidebar area------------------------------- */}
+        <div className={styles.sidebar}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure,
+          laborum?
+        </div>
+        {/* ---------------------------main area------------------------------- */}
+        <div className={styles.main}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam velit
+            porro pariatur harum quaerat qui in reprehenderit error minus amet
+            voluptatem enim aliquam numquam architecto, delectus eaque,
+            perferendis odio sint?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Preview;
