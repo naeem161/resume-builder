5fb26b6f9dcbc55786e4360561171a92664267ed
naeem161 <naeem.tahir2810@gmail.com>
2023-06-16 05:06:50 +0500

    make btn for changing resume sidebar bg & text color

--- src/Components/Preview.jsx
+++ src/Components/Preview.jsx
@@ -1,13 +1,19 @@
+import React from "react";
 import styles from "./Preview.module.css";
 import { useSelector } from "react-redux";
 import PhoneIcon from "../icons/phone.svg";
 import EmailIcon from "../icons/sign.svg";
 import LocationIcon from "../icons/location.svg";
+import { Input, Button, InputLabel } from "@mui/material";
 
 const Preview = () => {
+  const [sidebarBGColor, setSidebarBGColor] = React.useState("#959595");
+  const [sidebarColor, setSidebarColor] = React.useState("#FFFFFF");
   const { profile, skills, interests, work, education } = useSelector(
     (store) => store
   );
+
+  console.log(sidebarColor);
 
   // format date function
   function formatDate(utcDate) {
@@ -59,65 +65,115 @@ const Preview = () => {
   };
 
   return (
-    <div
-      className={styles.container}
-      style={{ margin: "40px auto", border: "2px solid red" }}
-    >
-      <div className={styles.resumeRow}>
-        {/* ---------------------------sidebar area------------------------------- */}
-        <div className={styles.sidebar}>
-          <h1
-            style={{ textAlign: "center" }}
-          >{`${profile.firstName} ${profile.lastName}`}</h1>
-          <div className={styles.contactSection}>
-            <img src={EmailIcon} className={styles.icon} />
-            {profile.phone}
-          </div>
-          <div className={styles.contactSection}>
-            <img src={PhoneIcon} className={styles.icon} />
-            {profile.email}
-          </div>
-          <div className={styles.contactSection}>
-            <img src={LocationIcon} className={styles.icon} />
-            {`${profile.city}, ${profile.country}`}
-          </div>
+    <>
+      <div
+        className={styles.container}
+        style={{ margin: "40px auto", border: "2px solid red" }}
+      >
+        <div className={styles.resumeRow}>
+          {/* ---------------------------sidebar area------------------------------- */}
+          <div
+            className={styles.sidebar}
+            style={{
+              backgroundColor: `${sidebarBGColor}`,
+              color: `${sidebarColor}`,
+            }}
+          >
+            <h1
+              style={{ textAlign: "center" }}
+            >{`${profile.firstName} ${profile.lastName}`}</h1>
+            <div className={styles.contactSection}>
+              <img src={EmailIcon} className={styles.icon} />
+              {profile.phone}
+            </div>
+            <div className={styles.contactSection}>
+              <img src={PhoneIcon} className={styles.icon} />
+              {profile.email}
+            </div>
+            <div className={styles.contactSection}>
+              <img src={LocationIcon} className={styles.icon} />
+              {`${profile.city}, ${profile.country}`}
+            </div>
 
-          {/* showing skill */}
+            {/* showing skill */}
 
-          <h2>Skills</h2>
-          <ul>
-            {skills.skills.map((skill, index) => (
-              <li key={index}> {skill} </li>
-            ))}
-          </ul>
+            <h2>Skills</h2>
+            <ul>
+              {skills.skills.map((skill, index) => (
+                <li key={index}> {skill} </li>
+              ))}
+            </ul>
 
-          {/* showing Interests */}
+            {/* showing Interests */}
 
-          <h2>Interest</h2>
-          <ul>
-            {interests.interests.map((interest, index) => (
-              <li key={index}> {interest} </li>
-            ))}
-          </ul>
-        </div>
-        {/* ---------------------------main area------------------------------- */}
-        <div className={styles.main}>
-          <h1 className={styles.catagory}> Summary </h1>
-          <p>{profile.summary}</p>
-          {/* experiences section */}
-          <div>
-            <h1 className={styles.catagory}>Experience</h1>
-            {work.experience &&
-              work.experience.map((exp) => populateExperience(exp))}
+            <h2>Interest</h2>
+            <ul>
+              {interests.interests.map((interest, index) => (
+                <li key={index}> {interest} </li>
+              ))}
+            </ul>
           </div>
-          {/* education section */}
-          <div>
-            <h1 className={styles.catagory}>Education</h1>
-            <ul>{education.education.map((edu) => populateEducation(edu))}</ul>
+          {/* ---------------------------main area------------------------------- */}
+          <div className={styles.main}>
+            <h1 className={styles.catagory}> Summary </h1>
+            <p>{profile.summary}</p>
+            {/* experiences section */}
+            <div>
+              <h1 className={styles.catagory}>Experience</h1>
+              {work.experience &&
+                work.experience.map((exp) => populateExperience(exp))}
+            </div>
+            {/* education section */}
+            <div>
+              <h1 className={styles.catagory}>Education</h1>
+              <ul>
+                {education.education.map((edu) => populateEducation(edu))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      {/* buttons for changing sidebar Backgroung & Text color */}
+      <div style={{ display: "flex", gap: 10 }}>
+        <div>
+          <Button variant="contained">
+            <InputLabel
+              htmlFor="bg-color"
+              sx={{
+                color: "white",
+              }}
+            >
+              Sidebar BG Color
+            </InputLabel>
+            <Input
+              type="color"
+              id="bg-color"
+              value={sidebarBGColor}
+              onChange={(e) => setSidebarBGColor(e.target.value)}
+            />
+          </Button>
+        </div>
+        <div>
+          <Button variant="contained">
+            <InputLabel
+              htmlFor="text-color"
+              sx={{
+                color: "white",
+              }}
+            >
+              Sidebar Text Color
+            </InputLabel>
+            <Input
+              type="color"
+              id="text-color"
+              value={sidebarColor}
+              onChange={(e) => setSidebarColor(e.target.value)}
+            />
+          </Button>
+        </div>
+      </div>
+    </>
   );
 };
 
